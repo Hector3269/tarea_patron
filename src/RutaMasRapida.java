5eea6156fe18b107966a135ff862b306ee1ad461
Hector3269 <danielramirezr27@gmail.com>
2024-07-29 12:14:14 -0600

    first commit

---
+++ src/RutaMasRapida.java
@@ -0,0 +1,6 @@
+public class RutaMasRapida implements RutaStrategy {
+    @Override
+    public String calcularRuta(String puntoA, String puntoB) {
+        return "Ruta más rápida entre " + puntoA + " y " + puntoB;
+    }
+}
